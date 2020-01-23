9d20e84ff5b9a66a350751c94e8689ab7ac754ed
dinesh24murali <dinesh24murali@gmail.com>
2020-01-23 09:47:58 +0530

    Add: My first django rest framework app;

---
+++ learn3/README.md
@@ -0,0 +1,8 @@
+### Environment setup
+
+You need to create a virtual environment with venv
+
+Need to pip install
+
+django
+djangorestframework
---
+++ learn3/learn3/music/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from rest_framework import generics
+from .models import Songs
+from .serializers import SongsSerializer
+
+
+class ListSongsView(generics.ListAPIView):
+    """
+    Provides a get method handler.
+    """
+    queryset = Songs.objects.all()
+    serializer_class = SongsSerializer
+
+def index(request):
+    return HttpResponse('Something')
+
+
