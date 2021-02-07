41626f402fb1e22fcf5f785afd7601df06bff4e8
dinesh24murali <dinesh24murali@gmail.com>
2021-02-07 12:51:50 +0530

    Fix: token_auth not working; Add: requirements txt file;

--- token_auth_example/token_backend/jwt_learn/settings.py
+++ token_auth_example/token_backend/jwt_learn/settings.py
@@ -129,13 +129,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    )
+    'PAGE_SIZE': 10
 }
 
 CORS_ORIGIN_ALLOW_ALL = False
